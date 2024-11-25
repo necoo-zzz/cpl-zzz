c69a9b63f280db4cc145ea9f25503f7146e41cbe
Ye <1047733914@qq.com>
2024-11-25 19:00:57 +0800

    incorrect version 1.0

---
+++ 6-data-base/brackets.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+#define MAX_SIZE 10005
+
+char stack[MAX_SIZE];
+int top = -1;
+
+void Push(char m) {
+    if (top < MAX_SIZE - 1) {
+        top++;
+        stack[top] = m;
+    }
+}
+
+void Pop() {
+    if (top != -1) {
+        top--;
+    }
+}
+
+void Clear(){
+    top = -1;
+}
+
+int main() {
+    int T = 0;
+    scanf("%d", &T);
+
+    for (int k = 0; k < T; k++){
+        char s[10005];
+        scanf("%s", s);
+
+        int i = 0;
+        int a = 0;
+        while(s[i] != '\0'){
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{'){
+                Push(s[i]);
+                a = 1;
+            }else{
+                switch(s[i]){
+                case ')':
+                    if (top != -1){
+                        if (stack[top] == ')') Pop();
+                        else
+                            printf("False\n");}else
+                                printf("False\n");
+                    break;
+                case ']':
+                    if (top != -1){
+                        if (stack[top] == ']') Pop();
+                        else
+                            printf("False\n");}else
+                                printf("False\n");
+                    break;
+                case '}':
+                    if (top != -1){
+                        if (stack[top] == '}') Pop();
+                        else
+                            printf("False\n");}else
+                                printf("False\n");
+                    break;
+                default:
+                    printf("False\n");
+                    break;
+                }
+            }
+            i++;
+        }
+        if (a == 1 && top == -1){
+            printf("True\n");
+        }
+    }
+    return 0;
+}
